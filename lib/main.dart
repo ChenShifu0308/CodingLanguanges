1a708fbe8a5e5345d47a23c9ae1993371c503a1b
Zheng Chen <zhengchen0308@gmail.com>
2023-11-05 09:15:09 -0700

    Init an empty app

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(primarySwatch: Colors.blue),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Flutter Demo Home Page'),
+          ),
+          body: const Center(
+            child: Text('Hello World'),
+          ),
+        ));
+  }
+}
